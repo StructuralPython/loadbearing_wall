49d61fd30be2fcf3c38e05f83eead0365165b18b
Connor Ferster <connorferster@gmail.com>
2025-09-05 09:05:06 -0700

    chore: bump version

--- src/loadbearing_wall/__init__.py
+++ src/loadbearing_wall/__init__.py
@@ -5,7 +5,7 @@ reactions and load-transfer.
 The wall model is parameterizable and can represent any material
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from loadbearing_wall.wall_model import LinearWallModel
 from loadbearing_wall import *
