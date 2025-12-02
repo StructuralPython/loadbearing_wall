a3f970b34eac70e37e96372a5c78d3f739f00bef
Connor Ferster <connorferster@gmail.com>
2025-12-01 16:11:40 -0800

    feat: add dedicated point loads; update logic

--- tests/test_geom.py
+++ tests/test_geom.py
@@ -2,10 +2,7 @@ from loadbearing_wall.geom_ops import apply_spread_angle
 
 
 def test_apply_spread_angle():
-    ret = apply_spread_angle(4, 3, spread_angle=10, w0=10, x0=1, w1=10, x1=2)
+    ret = apply_spread_angle(1, 4, spread_angle=45, w0=10, x0=1, w1=10, x1=2)
     assert ret == (
-        4.148317542163208,
-        4.148317542163208,
-        0.2946920771661401,
-        2.70530792283386,
+        3.333333333333333, 3.333333333333333, 0, 3
     )
